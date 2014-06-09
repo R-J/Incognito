1c399577be829ed0e543cac15a90760976da5be7
R-J <robin.jurinka@gmx.net>
2014-06-09 10:15:39 +0200

    Update class.incognito.plugin.php
    First working version. Needs code cleanup and commenting. Also implementation of new permissions

--- class.incognito.plugin.php
+++ class.incognito.plugin.php
@@ -3,9 +3,14 @@
 $PluginInfo['Incognito'] = array(
    'Name' => 'Incognito',
    'Description' => 'Allows users to post discussion and comments anonymously',
-   'Version' => '0.0.1',
+   'Version' => '0.1',
+   'RegisterPermissions' => array(
+      'Plugins.Incognito.Allow',
+      'Vanilla.Discussions.Incognito',
+      'Vanilla.Comments.Incognito'
+   ),
    'MobileFriendly' => TRUE,
-   'Author' =>' Robin',
+   'Author' =>' Robin Jurinka',
    'AuthorUrl' => 'http://vanillaforums.org/profile/44046/R_J',
    'License' => 'MIT'
 );
@@ -16,27 +21,58 @@ class IncognitoPlugin extends Gdn_Plugin {
    public function __construct() {
       $this->IncognitoUserID = C('Plugins.Incognito.UserID', Gdn::UserModel()->GetSystemUserID());
    }
+   
    public function Setup() {
       if (!C('Plugins.Incognito.UserID')) {
          SaveToConfig('Plugins.Incognito.UserID', Gdn::UserModel()->GetSystemUserID());
       }
    }
+   
    public function PostController_DiscussionFormOptions_Handler($Sender) {
-      $Sender->EventArguments['Options'] .= Wrap($Sender->Form->CheckBox('Incognito', 'Hide your name'), 'li');
-
+      if(Gdn::Session()->CheckPermission('Plugins.Incognito.Allow')) {
+         $Sender->EventArguments['Options'] .= Wrap($Sender->Form->CheckBox('Incognito', 'Hide your name'), 'li');
+      }
    }
+   
    public function DiscussionController_AfterBodyField_Handler($Sender) {
-      echo '<ul class="List Inline PostOptions><li>'.$Sender->Form->CheckBox('Incognito', 'Hide your name').'</li></ul>';
-   }
-   public function CommentModel_BeforeSaveComment_Handler($Sender) {
-      if ($Sender->EventArguments['FormPostValues']['Incognito'] == '1') {
-         $Sender->EventArguments['FormPostValues']['InsertUserID'] = $this->IncognitoUserID;
+      if(Gdn::Session()->CheckPermission('Plugins.Incognito.Allow')) {
+         echo '<ul class="List Inline PostOptions><li>'.$Sender->Form->CheckBox('Incognito', 'Hide your name').'</li></ul>';
       }
-   }
-   public function DiscussionModel_BeforeSaveDiscussion_Handler($Sender) {
-      if ($Sender->EventArguments['FormPostValues']['Incognito'] == '1') {
-         $Sender->EventArguments['FormPostValues']['InsertUserID'] = $this->IncognitoUserID;
-      }
+   
    }
 
+   public function CommentModel_BeforeSaveComment_Handler($Sender) {
+      $Session = Gdn::Session();
+      $DiscussionModel = new DiscussionModel();
+      $CategoryModel = new CategoryModel();
+      $DiscussionID = $Sender->EventArguments['FormPostValues']['DiscussionID'];
+      $Discussion = $DiscussionModel->GetID($DiscussionID);
+      $CategoryID = $Discussion->CategoryID;
+      $Category = $CategoryModel->GetID($CategoryID);
+      $PermissionCategoryID = $Category->PermissionCategoryID;
+      
+      if (
+         $Session->CheckPermission('Plugins.Incognito.Allow') && 
+         $Sender->EventArguments['FormPostValues']['Incognito'] == '1' &&
+         $Session->CheckPermission('Vanilla.Comments.Add', TRUE, 'Category', $PermissionCategoryID)
+      ) {
+         $Sender->EventArguments['FormPostValues']['InsertUserID'] = $this->IncognitoUserID;
+      }
+   }
+   
+   public function DiscussionModel_BeforeSaveDiscussion_Handler($Sender) {
+      $Session = Gdn::Session();
+      $CategoryModel = new CategoryModel();
+      $CategoryID = $Sender->EventArguments['FormPostValues']['CategoryID'];
+      $Category = $CategoryModel->GetID($CategoryID);
+      $PermissionCategoryID = $Category->PermissionCategoryID;   
+   
+      if (
+         $Session->CheckPermission('Plugins.Incognito.Allow') && 
+         $Sender->EventArguments['FormPostValues']['Incognito'] == '1' &&
+         $Session->CheckPermission('Vanilla.Comments.Add', TRUE, 'Category', $PermissionCategoryID)
+      ) {
+         $Sender->EventArguments['FormPostValues']['InsertUserID'] = $this->IncognitoUserID;
+      }
+   }
 }
