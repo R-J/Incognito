008f87efaaecdba1bb6a78b415ddb7f48a7b9e01
R-J <robin.jurinka@gmx.net>
2014-06-09 21:10:59 +0200

    Create definitions.php

---
+++ locale/de-DE/definitions.php
@@ -0,0 +1,3 @@
+<?php if (!defined('APPLICATION')) exit();
+$Definition['Hide your name'] = 'Namen verstecken';
+$Definition['You do not have permission to hide your name in this category'] = 'Du hast keine Berechtigung, deinen Namen in dieser Kategorie zu verstecken';
